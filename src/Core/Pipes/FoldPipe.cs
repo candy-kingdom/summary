8cca90e93e780d6e7720322fa01b09cf8146291d
Kostia Ohulchanskyi <k.ohulchanskyi@gmail.com>
2023-01-10 20:41:49 +0000

    Rename `FlattenPipe` to `FoldPipe`

---
+++ src/Core/Pipes/FoldPipe.cs
@@ -0,0 +1,15 @@
+﻿namespace Summary.Pipes;
+
+/// <summary>
+///     A <see cref="IPipe{I,O}"/> that aggregates the result of the specified pipe.
+/// </summary>
+public class FoldPipe<O> : IPipe<O[], O>
+{
+    private readonly Func<O, O, O> _fold;
+
+    public FoldPipe(Func<O, O, O> fold) =>
+        _fold = fold;
+
+    public Task<O> Run(O[] input) =>
+        Task.FromResult(input.Aggregate(_fold));
+}
