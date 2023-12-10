a330316396a3ad0d805ab650b50bc9fcba27b6c2
joshua-light <j.light.developer@gmail.com>
2023-12-10 15:04:56 +0200

    Docs

--- src/Core/AccessorKind.cs
+++ src/Core/AccessorKind.cs
@@ -3,6 +3,10 @@ namespace Summary;
 /// <summary>
 ///     The kind of <see cref="DocProperty"/> accessor.
 /// </summary>
+/// <remarks>
+///     We intentionally omit event accessors like <c>add</c> or <c>remove</c> since
+///     each event has both, and the modifiers are not supported by event accessors.
+/// </remarks>
 public enum AccessorKind
 {
     /// <summary>
